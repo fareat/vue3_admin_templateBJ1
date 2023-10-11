e700e1e105f028b2a48e22752c0734a2e4cb2bfe
fareat <2842994571@qq.com>
2023-10-11 18:19:03 +0800

    提交没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box">123456</div>
+</template>
+
+<script setup lang="ts">
+const str = '我爱你'
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
