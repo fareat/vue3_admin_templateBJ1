bea315ca1718ea7757e645ee9e11bac905af2d33
fareat <2842994571@qq.com>
2023-10-26 00:20:21 +0800

    完成品牌管理--分页功能

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -43,6 +43,9 @@
 
         
         <el-pagination
+          @size-change="sizeChange"
+          @current-change="getHasTrademark"
+          pager-count="5"
           v-model:current-page="pageNo"
           v-model:page-size="limit"
           :page-sizes="[3, 6, 9, 12]"
@@ -59,6 +62,7 @@
 <script setup lang="ts">
 import {ref,onMounted} from 'vue'
 import {reqHasTrademark} from '@/api/product/trademark/index'
+import type {Records,TradeMarkResponseData} from '@/api/product/trademark/type'
 //当前页码
 let pageNo=ref<number>(1)
 //每页展示多少条数据
@@ -66,22 +70,42 @@ let limit=ref<number>(3)
 //存储品牌数据总数
 let total=ref<number>(0)
 //存储已有品牌的数据
-let trademarkArr=ref<any>([])
+let trademarkArr=ref<Records>([])
 
 //获取已有品牌的接口封装为一个函数，在任何情况下获取数据调用函数即可
-const getHasTrademark=async ()=>{
-  let result= await reqHasTrademark(pageNo.value,limit.value)
+const getHasTrademark=async (pager=1)=>{
+  //为改变下拉菜单数据，重新赋值
+  pageNo.value=pager
+  let result:TradeMarkResponseData= await reqHasTrademark(pageNo.value,limit.value)
   //当生命周期钩子函数触发请求的时候，进行以下判断
   if(result.code==200){
     //存储已有品牌的总个数
     total.value=result.data.total
     trademarkArr.value=result.data.records
-  }
+  } 
 }
 //组件挂载完成钩子---发送一次请求，获取第一页，三个品牌数据
 onMounted(() => {
   getHasTrademark()
 })
+//分页器当前页码发生变化的时候会触发
+//对于当前页码发生变化的自定义事件，组件pagination给父组件回传了当前页码
+/* const changePageNo=()=>{
+  //当页码发生变化的时候再次发请求获取对应已有的品牌数据
+  getHasTrademark()
+   
+} */
+
+//当下拉菜单变化会触发这个方法
+//这个自定义事件，分页器组件会将下拉菜单选中的数据返回
+const sizeChange=(pagesize)=>{
+  //console.log(pagesize);
+  //下拉菜单，改变页面展示数据时
+  pageNo.value=1
+  getHasTrademark()
+  
+}
+
 </script>
 
 <style scoped></style>
